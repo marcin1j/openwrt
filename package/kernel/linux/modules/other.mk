59f34334bd5b402ba9a3d783043c429b52a3c3f3
Marcin Jurkowski <marcin1j@gmail.com>
2020-12-06 17:53:29 +0100

    ath79: add a custom TL-WR741 v1 device with w1 bus connected parallel to "RESET" button

--- package/kernel/linux/modules/other.mk
+++ package/kernel/linux/modules/other.mk
@@ -1233,3 +1233,20 @@ define KernelPackage/f71808e-wdt/description
 endef
 
 $(eval $(call KernelPackage,f71808e-wdt))
+
+
+define KernelPackage/connector
+  SUBMENU:=$(OTHER_MENU)
+  TITLE:=Netlink connector
+  KCONFIG:= \
+    CONFIG_CONNECTOR \
+    PROC_EVENTS=n
+  FILES:=$(LINUX_DIR)/drivers/connector/cn.ko
+  AUTOLOAD:=$(call AutoLoad,30,cn)
+endef
+
+define KernelPackage/connector/description
+  Kernel module for netlink userspace-kernelspace connector (cn.ko).
+endef
+
+$(eval $(call KernelPackage,connector))
